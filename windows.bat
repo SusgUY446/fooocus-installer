b355d7921964035d219d757635cccec7322a85b5
SusgUY446 <129160115+SusgUY446@users.noreply.github.com>
2024-02-16 09:02:01 +0100

    Update windows.bat
    installs cuda automaticly

--- windows.bat
+++ windows.bat
@@ -1,7 +1,9 @@
-echo Running Fooocus Installer Win-V1
+@echo off
+echo Running Fooocus Installer Win-V2
 
 git clone https://github.com/lllyasviel/Fooocus.git
+python3.11 -m venv venv
+.\venv\scripts\activate
 cd Fooocus
 pip install -r requirements_versions.txt
-
-
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
